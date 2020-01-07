8eacbc74ce5a6c41534b691ad5e26910a49c62e8
cherdon <liewcherdon@gmail.com>
2020-01-07 15:20:57 +0800

    Suggested Hard

---
+++ backend/helper/recommend.py
@@ -0,0 +1,9 @@
+suggestions = {
+    "limzeyang": {
+        "suggested": [
+            {"type": "SAVINGS", "name": ""}
+        ],
+        "credit_card": ["DBS Live Fresh Student Card"],
+        "debit_card": ["DBS Visa Debit Card", "PAssion POSB Debit Card"]
+    }
+}
